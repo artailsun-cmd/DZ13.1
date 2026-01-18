068c8f272369aef1957fc5116ea13b2234d82a17
AlexKarpany <artailsun@gmail.com>
2026-01-18 11:02:16 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DZ13._1;
+
+Account heikkisAccount = new Account("Heikki's account", 100.00);
+Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00);
+
+
+heikkisAccount.Withdrawal(20);
+Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.Balance);
+heikkisSwissAccount.Deposit(200);
+Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.Balance);
